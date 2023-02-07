317fc4e02caa7b9d529e71089b1672fbf72043a4
jamg <jamg44@gmail.com>
2023-02-07 20:50:51 +0100

    herencia simple

--- ejemplos/prototipos.js
+++ ejemplos/prototipos.js
@@ -12,3 +12,20 @@ Persona.prototype.saluda = function() { console.log('Hola soy', this.nombre) }
 
 pepe.saluda();
 luis.saluda();
+
+// Herencia simple ---------------------------------
+
+function Agente(nombre) {
+  // heredar el constructor de las personas
+  // llamar al constructor de las personas con mi this
+  Persona.call(this, nombre);
+}
+
+// heredar propiedades de las personas
+Agente.prototype = Object.create(Persona.prototype);
+Agente.prototype.constructor = Agente;
+
+const smith = new Agente('Smith');
+
+smith.saluda();
+
