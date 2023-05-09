21abc41b8ae5483c3f3781d5adf37feb970b4cb2
Javier <jamg44@gmail.com>
2023-05-09 19:27:31 +0200

    añadimos página features

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -34,6 +34,7 @@ app.use('/api/agentes', basicAuthMiddleware, require('./routes/api/agentes'));
  */
 app.use('/',      require('./routes/home'));
 app.use('/users', require('./routes/users'));
+app.use('/features', require('./routes/features'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
