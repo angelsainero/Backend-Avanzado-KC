7b676fe11434525f199338d9f1f4cc02827c0abe
Javier <jamg44@gmail.com>
2023-05-17 20:17:43 +0200

    creamos un servidor http

---
+++ ejemplos/ejemplo_websockets/httpServer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('Servidor HTTP arrancado en http://localhost:3000');
+})
